50b8a09c1a4b3be3fd317bff474852d5aaec95de
Eng-Abdul-Rehamn <eng.mirza.rehman@gmail.com>
2023-07-12 21:15:33 +0500

    frist commit

--- Components/Components/Footer.jsx
+++ Components/Components/Footer.jsx
@@ -9,12 +9,12 @@ export default function Footer() {
             <Box sx={{width: "80%", margin: "auto", display: {md:"flex", sm: "block"}, justifyContent: "space-between", paddingTop: 10, color: "white"}}>
                 <Box sx={{width: {md:"25%", sm: "60%", xs: "100%"}}}>
                     <Typography sx={{fontSize: 34,fontWeight: "bold"}}>Max Steel</Typography>
-                    <Typography sx={{color: "#9BA2AD", marginTop: 3}}>Ignissimos ducimus qui blanditiis prae sentium voluptatum deleniti.</Typography>
+                    <Typography sx={{color: "#9BA2AD", marginTop: 3}}>We deals in all kinds of stainless steel and aluminum</Typography>
                 </Box>
 
                 <Box sx={{width: {md:"14%", sm: "30%", xs: "50%"}}}>
                     <Typography sx={{fontSize: 24,fontWeight: "bold"}}>Office</Typography>
-                    <Typography sx={{color: "#9BA2AD", marginTop: 3}}>Germany — <br/> 785 15h Street, Office 478 Berlin, De 81566.</Typography>
+                    <Typography sx={{color: "#9BA2AD", marginTop: 3}}>Gujrawala — <br/> Calimix town 7 no Street, Office shahinabard</Typography>
                     <Typography sx={{color: "#9BA2AD", marginTop: 2}}>maxsteel.construction@gamil.com</Typography>
                     <Typography sx={{color: "white", marginTop: 2, fontSize: 24, fontWeight: "bold", width: 200}}>+92 305 4204805</Typography>
                 </Box>
