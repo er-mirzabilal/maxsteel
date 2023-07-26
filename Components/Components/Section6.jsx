8872d73cf74077bb4768ff37e520901cf2fa467b
Eng-Abdul-Rehamn <eng.mirza.rehman@gmail.com>
2023-07-26 10:54:16 +0500

    done

--- Components/Components/Section6.jsx
+++ Components/Components/Section6.jsx
@@ -6,10 +6,12 @@ export default function Section6 () {
         <>
         <Box sx={{backgroundColor: "#1C1E36", width: "100%",height: {lg: 300, md:480, sm: 500, xs: "100%"}, paddingBottom: {sm:0, xs: 10}}}>
             <Grid container gap={10} sx={{width: "80%", margin: "auto", justifyContent: "center", alignItems: "center", paddingTop: 8}}>
+                <Link to="/ClientDetail">
                 <Box sx={{textAlign: "center"}}>
                     <Typography sx={{fontSize: 120, opacity: 0.1, color: "white", zIndex: 0}}>50</Typography>
                     <Typography sx={{marginTop: -13, fontSize: 20, fontWeight: "bold", color: "white", zIndex: 1}}>Projects</Typography>
                 </Box>
+                </Link>
                 <Box sx={{display: {sm:"block", xs: "none"} ,width: "2px", height: 40, backgroundColor: "#595959", marginTop: 10}}></Box>
                 <Box sx={{textAlign: "center"}}>
                     <Typography sx={{fontSize: 120, opacity: 0.1, color: "white", zIndex: 0}}>10</Typography>
